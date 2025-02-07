9caa2c380ef87acabf25e65deb4af5ea6dfebc0a
hrysag <hrysag@gmail.com>
2025-02-08 00:53:01 +0800

    test for new

---
+++ Hello.js
@@ -0,0 +1,5 @@
+console.log('hello world');
+function add(a, b) {
+    console.log(a + b);
+}
+add(1, 2);
---
+++ Hello.ts
@@ -0,0 +1,9 @@
+console.log('hello world');
+
+function add(a: number, b: number) 
+{
+
+    console.log(a + b);
+}
+
+add(1, 2);
