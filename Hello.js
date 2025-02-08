c6785d6617c5f6be7a5d6dca0ebc7a4cd7a38083
hrysag <hrysag@gmail.com>
2025-02-09 02:47:54 +0800

    測試新增到stage和commit

--- Hello.js
+++ Hello.js
@@ -2,4 +2,7 @@ console.log('hello world');
 function add(a, b) {
     console.log(a + b);
 }
+function theotherfunction() {
+    console.log('this is the other function---20250209');
+}
 add(1, 2);
--- Hello.ts
+++ Hello.ts
@@ -6,4 +6,8 @@ function add(a: number, b: number)
     console.log(a + b);
 }
 
+function theotherfunction()
+{
+    console.log('this is the other function---20250209');
+}
 add(1, 2);
